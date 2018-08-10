986dd47439b6153397667e5a867a617674f18757
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-08-10 16:34:13 -0400

    end of day Friday

--- script.js
+++ script.js
@@ -1,36 +1,136 @@
-console.log('Add validation!');
+console.log('Add validation!'); // just to make sure I'm working :)
 
 // Each field is required. If there is no user input, mark it as invalid
 // identify fields with a var
+
 // need event listener for the form -- call will begin upon submit
-document.getElementById('parking-form').addEventListener('submit', function (event){
+document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
-    // check()
 
-// var for the input fields -- these can't be blank
-var nameField = document.getElementById('name').value
-    console.log(nameField)
+    // clearError()
 
-    if (nameField === '') {
-        dispEmptyError ()
-    } 
+    // var for the input fields -- these are all required fields .value pulls the value of 'name'
+    // check to see if the input fields are empty .value
+    var name = document.getElementById('name').value
+    var carYear = document.getElementById('car-year').value
+    var carMake = document.getElementById('car-make').value
+    var carModel = document.getElementById('car-model').value
+    var dateParking = document.getElementById('start-date').value
+    var numOfDays = document.getElementById('days').value
+    var creditCard = document.getElementById('credit-card').value
+    var cvv = document.getElementById('cvv').value
+    var expiration = document.getElementById('expiration').value
+
+    if (name === '') {
+        emptyNameError()
+    } //else turn green; will need to create a function for not empty
+
+    if (carYear === '') {
+        emptyCarError()
+    }
+
+    if (carMake === '') {
+        emptyCarError()
+    }
+    
+    if (carModel === '') {
+        emptyCarError()
+    }
+
+    if (dateParking === '') {
+        emptyDateError()
+    }
+
+    if (numOfDays === '') {
+        emptyDaysError()
+    }
+
+    if (creditCard === '') {
+        emptyCreditError()
+    }
+
+    if (cvv === '') {
+        emptyCvvError()
+    }
+
+    if (expiration === '') {
+        emptyExpError ()
+    }
+
 })
 
-// function check(){
-//     var inputField = document.getElementsByClassName('input-field')
-// console.log(inputField)
-// }
-
-// check to see if the input fields are empty, using var nameField
 
 // define function for disp empty fields; what is the message
-function dispEmptyError () {
+// function dispEmptyError creates class='input-invalid' in div tag
+function emptyNameError() {
+    var errorMessDiv = document.createElement('div') // only need these two for valid
+    errorMessDiv.classList.add('input-invalid') // ony need these two for valid
+    errorMessDiv.innerText = 'Required field'
+
+    // this appends the errorMessDiv to the 'name-field' element, adding the 'input-invalid' class
+    // which results in error 'Req field cannot be blank' if the field is empty
+    var nameField = document.getElementById('name-field')
+    nameField.appendChild(errorMessDiv)
+    nameField.classList.add('input-invalid')
+}
+
+function emptyCarError () {
     var errorMessDiv = document.createElement('div')
     errorMessDiv.classList.add('input-invalid')
-    errorMessDiv.innerText = 'Required field cannot be blank'
+    errorMessDiv.innerText = 'Required Field'
 
-    var fields = document.getElementById('name-field')
-    fields.appendChild(errorMessDiv)
-    fields.classList.add('input-invalid')
+    var carFields = document.getElementById('car-field')
+    carFields.appendChild(errorMessDiv)
+    carFields.classList.add('input-invalid')
+}
+
+function emptyDateError () {
+    var errorMessDiv = document.createElement('div')
+    errorMessDiv.classList.add('input-invalid')
+    errorMessDiv.innerText = 'Required field'
+
+    var dateField = document.getElementById('start-date-field')
+    dateField.appendChild(errorMessDiv)
+    dateField.classList.add('input-invalid')
+}
+
+function emptyDaysError () {
+    var errorMessDiv = document.createElement('div')
+    errorMessDiv.classList.add('input-invalid')
+    errorMessDiv.innerText = 'Required field'
+
+    var daysField = document.getElementById('days-field')
+    daysField.appendChild(errorMessDiv)
+    daysField.classList.add('input-invalid')
+}
+
+function emptyCreditError () {
+    var errorMessDiv = document.createElement('div')
+    errorMessDiv.classList.add('input-invalid')
+    errorMessDiv.innerText = 'Required field'
+
+    var creditCardField = document.getElementById('credit-card-field')
+    creditCardField.appendChild(errorMessDiv)
+    creditCardField.classList.add('input-invalid')
+}
+
+function emptyCvvError () {
+    var errorMessDiv = document.createElement('div')
+    errorMessDiv.classList.add('input-invalid')
+    errorMessDiv.innerText = 'Required Field'
+
+    var cvvField = document.getElementById('cvv-field')
+    cvvField.appendChild(errorMessDiv)
+    cvvField.classList.add('input-invalid')
+}
+
+function emptyExpError () {
+    var errorMessDiv = document.createElement('div')
+    errorMessDiv.classList.add('input-invalid')
+    errorMessDiv.innerText = 'Required Field'
+
+    var expField = document.getElementById('expiration-field')
+    expField.appendChild(errorMessDiv)
+    expField.classList.add('input-invalid')
 }
 
